c3059841fe7afa190b924043ad262214a8d08e5a
1mache <miktor4925@gmail.com>
2025-02-23 08:53:05 +0200

    Added constants header

---
+++ Constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace Constants
+{
+	constexpr size_t IMG_WIDTH = 600;
+	constexpr size_t IMG_HEIGHT = 400;
+
+	// Binary format of the PPM file
+	// from https://en.wikipedia.org/wiki/Netpbm#File_formats "P3" means this is a RGB color image in ASCII
+	constexpr const char* BIN_FORMAT = "P3";
+}
