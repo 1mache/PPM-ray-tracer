8831d761cd6a5ffe0a1b048462386b2ca84a2447
1mache <miktor4925@gmail.com>
2025-02-23 09:12:33 +0200

    Simple file creation logic

--- Constants.h
+++ Constants.h
@@ -5,6 +5,8 @@ namespace Constants
 	constexpr size_t IMG_WIDTH = 600;
 	constexpr size_t IMG_HEIGHT = 400;
 
+	constexpr const char* OUTPUT_FILE_NAME = "output.ppm";
+
 	// Binary format of the PPM file
 	// from https://en.wikipedia.org/wiki/Netpbm#File_formats "P3" means this is a RGB color image in ASCII
 	constexpr const char* BIN_FORMAT = "P3";
--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <fstream>
 
+#include "Constants.h"
+
+void setPixels(std::ofstream& outputFile)
+{
+	std::cout << "Generating...";
+}
+
+bool generateImage()
+{
+	std::ofstream outputFile(Constants::OUTPUT_FILE_NAME);
+	if(!outputFile.is_open())
+	{
+		std::cout << "Something went wrong! Couldn`t open file";
+		return false;
+	}
+
+	outputFile << Constants::BIN_FORMAT << std::endl;
+	outputFile << Constants::IMG_WIDTH << ' ' << Constants::IMG_HEIGHT << std::endl;
+
+	setPixels(outputFile);
+	outputFile.close();
+}
 int main(int argc, char* argv[])
 {
-	std::cout << "Hi Im a ray tracer";
+	generateImage();
+	std::cout << "Done!";
 }
