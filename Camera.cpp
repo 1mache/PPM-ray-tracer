d3f15d34bb318ffabebcbe5d960732dc8e77ae65
1mache <miktor4925@gmail.com>
2025-03-01 16:40:33 +0200

    Added camera class and moved some logic and data to from the ImageGenerator

---
+++ Camera.cpp
@@ -0,0 +1,24 @@
+#include "Camera.h"
+
+Camera::Camera(const Vec3& position, float FOV, float viewportDist, float screenAspectRatio):
+	m_position(position), m_FOV(FOV), m_viewportDist(viewportDist)
+{
+	// height calculated from FOV using famous formula
+	m_viewportHeight = 2 * (tan(FOV / 2) * viewportDist); // see explanation in materials
+	// width is calculated from height using aspect ratio
+	m_viewportWidth = m_viewportHeight * screenAspectRatio;
+
+	m_horizontal = Vec3(m_viewportWidth, 0, 0);
+	m_vertical = Vec3(0, m_viewportHeight, 0);
+	// y positive is up, x positive is right
+	m_lowLeftCorner = Vec3(-m_viewportWidth / 2, -m_viewportHeight / 2, -viewportDist);
+}
+
+Ray Camera::getRay(float xRatio, float yRatio) const
+{
+	// using the ratio translate screen point to viewport point
+	Vec3 viewportPoint = m_lowLeftCorner + (xRatio * m_horizontal) + (yRatio * m_vertical);
+
+	// cast ray from cam position to the point we calculated
+	return Ray(m_position, viewportPoint);
+}
---
+++ Camera.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Vec3.h"
+#include "Ray.h"
+
+class Camera
+{
+	// how far the viewport is from the camera (to the Z negative direction)
+	const float m_viewportDist;
+	// the angle between the TOP and BOTTOM edges of the viewport
+	const float m_FOV; // (in radians)
+
+	Vec3 m_position;
+	// the width and height of the "window" we're looking at the world through
+	// calculated using FOV
+	float m_viewportWidth, m_viewportHeight;
+
+	// positive horizontal and vertical viewport dimensions as vectors
+	Vec3 m_horizontal, m_vertical;
+	Vec3 m_lowLeftCorner;
+public:
+	Camera(const Vec3& position, float FOV, float viewportDist, float screenAspectRatio);
+
+	Vec3 position() const { return m_position; }
+	float viewportWidth() const { return m_viewportWidth; };
+	float viewportHeight() const { return m_viewportHeight; };
+
+	Ray getRay(float xRatio, float yRatio) const;
+};
+
