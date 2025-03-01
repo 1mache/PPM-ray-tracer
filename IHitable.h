054628590869f02a2e51a10ec92b7ece1da0734d
1mache <miktor4925@gmail.com>
2025-03-01 14:55:47 +0200

    Forgot virtual destructor at IHitablle

--- IHitable.h
+++ IHitable.h
@@ -12,6 +12,6 @@ struct HitRecord
 class IHitable
 {
 public:
-	~IHitable() {};
+	virtual ~IHitable() {};
 	virtual bool isHit(const Ray& ray, float tMin, float tMax, HitRecord& out_record) const = 0;
 };
