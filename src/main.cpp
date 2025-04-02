3321661790f295c54a9169d5ca12551c10f719c3
1mache <miktor4925@gmail.com>
2025-04-02 17:39:51 +0300

    Changed ground color and generalize sphere creation

--- src/main.cpp
+++ src/main.cpp
@@ -5,27 +5,28 @@
 
 int main(int argc, char* argv[])
 {
-	auto greenishAlbedo = Vec3(0.5f, 0.7f, 0.2f);
-	auto greenishMatte = std::make_shared<Lambertian>(greenishAlbedo);
+	auto grayAlbedo = Vec3(0.2f, 0.2f, 0.2f);
+	auto floorMaterial = std::make_shared<Lambertian>(grayAlbedo);
 	
 	using rng = Utils::RNG;
 	Interval posInterval(-5.0f, 5.0f);
 	Interval radiusInterval(0.1f, 0.5f);
-	std::vector<IHitable*> spheres(51);
+	uint8_t sphereCount = 50;
+	std::vector<IHitable*> spheres(sphereCount + 1);
 	SphereSpawner spawner;
 
-	for(int i = 0; i < 50; i++)
+	for(int i = 0; i < sphereCount; i++)
 	{
 		spheres[i] = spawner.spawnRandomSphere(posInterval, radiusInterval);
 	}
 
-	spheres[50] = new Sphere({ 0.0f, -100.5f,  -2.0f }, 100.0f, greenishMatte); // ground
+	spheres[sphereCount] = new Sphere({ 0.0f, -100.5f,  -2.0f }, 100.0f, floorMaterial); // ground
 	HitableSet world(std::move(spheres));
 
 	auto screenDimensions = Dimensions(800, 600);
     Camera camera = Camera(
 		screenDimensions,
-		{ 0.0f, 0.0f, 1.0f }, // position
+		{ -1.0f, 0.8f, 1.0f }, // position
 		{ 0.0f, 0.0f, -1.0f }, // direction
 		M_PI / 1.5 ); // vertical field of view		
 
