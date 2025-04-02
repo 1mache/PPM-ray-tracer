51c0a24e1e11f7ee5556f13a152d30e389c8a1fe
1mache <miktor4925@gmail.com>
2025-04-02 22:41:13 +0300

    Old main + its output added

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,15 @@
 
 int main(int argc, char* argv[])
 {
-	auto greenishAlbedo = Vec3(0.5f, 0.7f, 0.2f);
+	auto greenishAlbedo = Vec3(0.0f, 0.6f, 0.4f);
 	auto reddishAlbedo =  Vec3(0.7f, 0.2f, 0.2f);
 	auto purpleAlbedo =   Vec3(0.9f, 0.4f, 0.9f);
-	auto greenishAlbedo = Vec3(0.5f, 0.7f, 0.2f);
 	auto metalAlbedo =    Vec3(1.0f, 0.8f, 1.0f);
 
 	auto reddishMatte  =    std::make_shared<Lambertian>(reddishAlbedo);
 	auto greenishMatte =    std::make_shared<Lambertian>(greenishAlbedo);
 	auto purpleMatte   =    std::make_shared<Lambertian>(purpleAlbedo);
-	auto fuzzyMetal    =    std::make_shared<Metal>(metalAlbedo, 0.5f);
+	auto fuzzyMetal    =    std::make_shared<Metal>(metalAlbedo, 0.2f);
 	auto clearMetal    =    std::make_shared<Metal>(metalAlbedo, 0.0f);
 	auto glass		   =	std::make_shared<Dielectric>(1.5f);
 	auto bubble        =	std::make_shared<Dielectric>(1.0f/1.5f);
@@ -31,9 +30,9 @@ int main(int argc, char* argv[])
 	auto screenDimensions = Dimensions(800, 600);
     Camera camera = Camera(
 		screenDimensions,
-		{ 0.0f, 0.0f, 1.0f }, // position
-		{ 0.0f, 0.0f, -1.0f }, // direction
-		M_PI / 1.5 ); // vertical field of view		
+		{ 0.2f, 0.0f, 1.0f }, // position
+		{ 0.2f, 0.0f, -1.0f }, // direction
+		M_PI / 1.8f ); // vertical field of view		
 
 	ImageGenerator generator(screenDimensions ,world, camera);
 	
